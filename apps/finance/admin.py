649d75af78c117656ad6baa1a57aeaf83baf5e24
obrunodev <brunorpdev@gmail.com>
2022-06-27 15:17:13 -0300

    :sparkles: feat: Models created

--- apps/finance/admin.py
+++ apps/finance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Spending
+
+admin.site.register(Category)
+admin.site.register(Spending)
--- apps/finance/models.py
+++ apps/finance/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nome')
+
+    def __str__(self):
+        return self.name
+
+
+class Spending(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, verbose_name='Descrição')
+    expected_cost = models.FloatField(verbose_name='Custo esperado')
+    real_cost = models.FloatField(verbose_name='Custo real')
+    difference = models.FloatField(verbose_name='Diferença')
+
+    def __str__(self):
+        return self.description
