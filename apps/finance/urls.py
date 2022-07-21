795a7877005d7e1a62adba5f11b6f49f63f44486
Bruno R. Pianca <brunorpdev@gmail.com>
2022-07-21 01:37:34 -0300

    :sparkles: feat: Added a management page

--- apps/finance/urls.py
+++ apps/finance/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.dashboard_index, name='dashboard_index'),
 
     path('income/', views.income_index, name='income_index'),
+    path('income/manage/', views.income_manage, name='income_manage'),
 ]
--- apps/finance/views.py
+++ apps/finance/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from finance.models import Income
+from finance.models import Category, Income
 
 
+# VIEWS DE DASHBOARD
 def dashboard_index(request):
     '''Carrega a dashboard'''
     return render(request, 'finance/dashboard/index.html')
 
 
+# VIEWS DE RENDA MENSAL
 def income_index(request):
     '''Renderiza as tabelas de renda da família'''
     income = Income.objects.order_by('day_recurrency')
@@ -15,5 +17,12 @@ def income_index(request):
     if request.method == 'GET':
         context['total_expected_value'] = sum([item.expected_value for item in income])
         context['total_real_value'] = sum([item.real_value for item in income])
-        context['relation'] = context['total_expected_value'] - context['total_real_value']
+        context['relation'] = context['total_real_value'] - context['total_expected_value']
     return render(request, 'finance/income/index.html', context)
+
+
+def income_manage(request):
+    '''Tela de gestão de rendas'''
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'finance/income/manage.html')
